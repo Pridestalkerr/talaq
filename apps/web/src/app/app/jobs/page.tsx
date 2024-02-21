af9f4de1d2c1cf97fd5ed9325bbdfa8dd7372016
pridestalkerr <danartenie158@gmail.com>
2024-02-22 01:44:10 +0200

    cleaned up some unused vars on jobs page

--- apps/web/src/app/app/jobs/page.tsx
+++ apps/web/src/app/app/jobs/page.tsx
@@ -38,86 +38,12 @@ import {
 } from "lucide-react";
 import { toast } from "react-toastify";
 import Link from "next/link";
-import { z } from "zod";
+import { Job, jobCols } from "@/components/shared";
 
-type Job = RouterOutputs["jobsRouter"]["search"]["records"][number];
 type PageProps = {};
 
-const jdSchema = z.object({
-  autoReqId: z.string(),
-  srNo: z.string(),
-  lobDetails: z.string(),
-  reportingManager: z.string(),
-  requisitionStatus: z.string(),
-  tagManager: z.string().nullable(),
-  primarySkill: z.string(),
-  secondarySkill: z.string().nullable(),
-  infraDomain: z.string().nullable(),
-  customerName: z.string(),
-  band: z.string(),
-  subBand: z.string(),
-  designation: z.string(),
-  experience: z.string().nullable(),
-  jobDescription: z.string(),
-  jobDescriptionPost: z.string().nullable(),
-  country: z.string(),
-  requisitionSource: z.string().nullable(),
-  billingType: z.string(),
-  noOfPositions: z.number().nullable(),
-  positionsBalance: z.number().nullable(),
-  actionablePositions: z.number().nullable(),
-  tp1Interviewer: z.string().nullable(),
-  tp2Interviewer: z.string().nullable(),
-  companyCode: z.string(),
-  initiatorId: z.coerce.string(),
-  sla: z.number().nullable(),
-  agingInDays: z.number().nullable(),
-});
-
-const lookup = {
-  autoReqId: "Auto req ID",
-  srNo: "SR Number",
-  lobDetails: "LoB Details",
-  reportingManager: "Reporting Manager [vReportingManager1]",
-  requisitionStatus: "Reqisition Status",
-  tagManager: "TAG Manager [TAGManager]",
-  primarySkill: "Primary Skill",
-  secondarySkill: "Secondary Skill",
-  infraDomain: "Domain for INFRA",
-  customerName: "Customer Name",
-  band: "Band [iBandId]",
-  subBand: "Sub Band [vSubBandId]",
-  designation: "Designation",
-  experience: "Experience [iExperienceId]",
-  jobDescription: "Job Description",
-  jobDescriptionPost: "Job Description (Posting) [JD_ForPosting]",
-  country: "Country",
-  requisitionSource: "Requisition Source [iRequistionSource]",
-  billingType: "Billing Type [iBillingTypeId]",
-  noOfPositions: "No. of Positions",
-  positionsBalance: "Balance Positions",
-  actionablePositions: "Actionable Positions [Vacancies]",
-  tp1Interviewer: "TP1 Interveiwer",
-  tp2Interviewer: "TP2 Interveiwer",
-  companyCode: "Company Code [vcomp_code]",
-  initiatorId: "Initiator Id",
-  sla: "SLA",
-  agingInDays: "Ageing in Days",
-} as const satisfies {
-  [key in keyof z.infer<typeof jdSchema>]: string;
-};
-
-const metaCols = Object.keys(jdSchema.shape).map((key) => {
-  const k = key as keyof typeof lookup;
-  return {
-    name: lookup[k],
-    get: (job: Job) => job.meta?.[k] ?? "N/A",
-    identifier: k,
-  };
-});
-
 const columns = [
-  ...metaCols,
+  ...jobCols,
   { name: "", get: (job: Job) => job.id, identifier: "actions" },
 ] as const;
 
